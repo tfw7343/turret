6d81d00f7be8dafed7f95fc1b717a9db3ae64828
CYK-7 <36565225+CYK-7@users.noreply.github.com>
2019-11-17 21:17:31 +0800

    Update turret.ino

--- turret.ino
+++ turret.ino
@@ -1,23 +1,38 @@
 //arduino uno
-
 #include <Servo.h> 
+#include <stdlib.h>
 
-int LED;
+
+String str;
+
+int LED = 6;
 Servo servoX;
+String ascii_servoX;
+String ascii_servoY;
+
 
 void setup() { 
-  Serial.begin(9600); 
-  myservo.attach(5);
+  Serial.begin(9600);
+  servoX.attach(5);
+  servoX.write(0);
 } 
 
 void serialEvent() {
-  cords = Serial.readString();
 
-  servoX.write(cords);
-
-
-}
-
-void loop() {
-
+  String num1 = Serial.readString();
+  int num = num1.toInt();
+  
+  if (num1 == "420") {
+    analogWrite(LED, 0);
+    }
+  if (num1 == "69") {
+    analogWrite(LED, 255);
+    }
+  if (num1 == "666") {
+    
+    }
+  Serial.println(num1);
+  
+  servoX.write(num);
+  
 }
