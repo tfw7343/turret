b2c4898f425847c4a828ed232df11903f0603f02
CYK-7 <36565225+CYK-7@users.noreply.github.com>
2019-11-17 21:16:57 +0800

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -5,26 +5,28 @@ import time
 import sys
 import numpy as np
 import math
-
-
-# using arduino uno
-# ser = serial.Serial("/dev/ttyUSB0")  # open serial port
-# ser = serial.Serial("COM3",timeout=1)
+import serial
+# ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 # print(ser.name)         # check which port was really used
-
-ports = ['COM' + i+1 for i in range(20)]#goes through ports
 
 framerate = 30
 
-
-try ser = serial.Serial("/dev/cu.usbserial-1410",timeout=1):
-    except serial.SerialException:
-        ser = serial.Serial(ports, timeout=1)
+a = 0
+b = 1
+ser = serial.Serial("/dev/cu.usbserial-1410",timeout=1)
 
 def sendData(data):
     #ser.write(bytes(data))
     print("")
+    #if x == lower than 180...
 
+
+"""
+radius = 60;
+angle  = 140;
+xthing = radius * Math.sin(Math.PI * 2 * angle / 360);
+ything = radius * Math.cos(Math.PI * 2 * angle / 360);
+print('Points coors are  x='+ math.round(x * 100) / 100 +', y=' + math.round(y * 100) / 100)"""
 
 # formula to detect the co-ords of the face in the camera
 # x_face = radius * Math.sin(Math.PI * 2 * angle / 360);
@@ -42,34 +44,39 @@ radius = 10
 
 #haarcascade = https://pysource.com/wp-content/uploads/2018/10/haarcascades.zip
 
-def nothing():
+def nothing(x):
     pass
 
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS, 60)
+
 
 cv2.namedWindow("Frame")
+cv2.namedWindow("Camera_View")
 cv2.createTrackbar("Neighbours", "Frame", 5, 20, nothing)
 
 while True:
-
+    fps = int(cap.get(5))
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     neighbours = cv2.getTrackbarPos("Neighbours", "Frame")
 
     faces = face_cascade.detectMultiScale(gray, 1.3, neighbours)
+    cv2.imshow("Camera_VIEW", Camera_view)
     for rect in faces:
 
         (x, y, w, h) = rect
         bottomLeftCornerOfText = (x,y)
 
-        p1 = (x, y )#top right point
+        p1 = (x, y )
         p2 = (x + w // 2, y + h) #bottom point
-        p3 = (x + w, y ) #top left point
+        p3 = (x + w, y ) #top line
 
         # Drawing the triangle
+        # on the black window With given points
         cv2.line(frame, p1, p2, (0, 0, 255), 3)
         cv2.line(frame, p2, p3, (0, 0, 255), 3)
         cv2.line(frame, p1, p3, (0, 0, 255), 3)
@@ -77,13 +84,17 @@ while True:
 
         cv2.putText(frame,"x = " + str(x) + " y= " + str(y), bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
         cv2.imshow("Frame", frame)
-        
-        ser.sendData(x)
-        #degrees = math.atan2(y,x)/math.pi * 180 
+
+
+        degrees = math.atan2(y,x)/math.pi * 180
+        print(x / 5)
         #print("y position = " + str(round(degrees, 1)))
+        time.sleep(0.1)
 
+        ser.write(x)
 
-        # (X, Y) = (x1 + x2 + x3//3, y1 + y2 + y3//3)
+        #         # (X, Y) = (x1 + x2 + x3//3, y1 + y2 + y3//3)
+        centroid = ((p1[0]+p2[0]+p3[0])//3, (p1[1]+p2[1]+p3[1])//3)
 
         # detects the angle of the face on the circle using y
 
@@ -91,7 +102,14 @@ while True:
         if key == 32:#32 = space key
             ser.close()
             break
-        
+        # if key == 108:
+        #     if a == 0:
+        #         ser.write("LED_ON\n")
+        #         a == 1
+        #     if a == 1:
+        #         ser.write("LED_OFF\n")
+        #         a == 0
 
 cap.release()
 cv2.destroyAllWindows()
+ser.close()
